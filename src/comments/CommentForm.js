6229f0d18baf9aa290ae31424ae98f6efeb8f020
hoanghai86 <hainguyenlehoang86.gmail.com>
2023-02-09 16:34:34 +0700

    ''

---
+++ src/comments/CommentForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const CommentForm = ({ handleSubmit, submitLabel }) => {
+  const [text, setText] = useState("");
+  const isTextareaDisable = text.length === 0;
+  const onSubmit = (event) => {
+    event.preventDefault();
+    handleSubmit(text);
+    setText("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <textarea
+        className="comment-form-textarea"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <button className="comment-form-button" disabled={isTextareaDisable}>
+        {submitLabel}
+      </button>
+    </form>
+  );
+};
+
+export default CommentForm;
---
+++ src/comments/Comments.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import {
+  getComments as getCommentsApi,
+  createComment as createCommentApi,
+} from "../Api";
+import Comment from "./Comment";
+import CommentForm from "./CommentForm";
+
+const Comments = ({ currentUserId }) => {
+  const [backendComments, setBackendComments] = useState([]);
+  const rootComments = backendComments.filter(
+    (backendComment) => backendComment.parentId === null
+  );
+
+  const getReplies = (commentId) => {
+    return backendComments
+      .filter((backendComment) => backendComment.parentId === commentId)
+      .sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+      );
+  };
+
+  const addComment = (text, parentId) => {
+    console.log("addComment", text, parentId);
+    createCommentApi(text, parentId).then((comment) => {
+      setBackendComments([comment, ...backendComments]);
+    });
+  };
+  
+  console.log(backendComments)
+
+  useEffect(() => {
+    getCommentsApi().then((data) => {
+      setBackendComments(data);
+    });
+  }, []);
+
+  return (
+    <div className="comments">
+      <h3 className="comments-title">Comments</h3>
+      <div className="comment-form-title">Write comment</div>
+      <CommentForm submitLabel="Write" handleSubmit={addComment} />
+      <div className="comments-container">
+        {rootComments.map((rootComment) => (
+          <div key={rootComment.id}>
+            <Comment
+              key={rootComment.id}
+              comment={rootComment}
+              replies={getReplies(rootComment.id)}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
